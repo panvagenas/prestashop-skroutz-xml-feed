69dc06ed077bb14b1c6681b136ec7f3b6b499979
Panagiotis Vagenas <pan.vagenas@gmail.com>
2015-04-18 19:53:26 +0300

    screenshots

--- SkroutzXML/Module.php
+++ SkroutzXML/Module.php
@@ -40,7 +40,7 @@ class Module extends \XDaRk_v150216\Module{
 	/**
 	 * @var string
 	 */
-	public $author = '<a href="http://erp.xdark.eu" target="_blank">Panagiotis Vagenas</a>';
+	public $author = 'Panagiotis Vagenas';
 	/**
 	 * @var int
 	 */
